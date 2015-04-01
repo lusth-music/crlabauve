81fbce29d330b1c2b639ab7c667d1f6298934c43
Christopher LaBauve <jiminychris@gmail.com>
2015-04-01 11:05:05 -0500

    Modifying test to play several notes

--- uketest.c
+++ uketest.c
@@ -17,14 +17,14 @@ void riff(int, int);
 int main()
 {
     int instrument;
-    int octave = 4;
+    int octave;
     int i;
 
     songInit();
 
     instrument = readScale(dir,base);
 
-    setTempo(154);
+    setTempo(120);
     setTime(4,4);
     setStride(0.05);
     setSustain(0.99995);
@@ -32,48 +32,11 @@ int main()
 
     openOutput("uketest.rra",0,0);
 
-    for (i = 0; i < 4; i++)
-        riff(instrument, octave);
-
-    /*c(6, Hd, instrument, octave);
-    b(6, Q, instrument, octave, "--x", "-x-", "x--", SX);
-    c(4, Q, instrument, octave);
-    b(4, Q, instrument, octave, "x--", "-x-", SX);
-    c(1, H, instrument, octave);
-    c(5, H, instrument, octave);
-    b(5, H, instrument, octave, "x--", "--x", "-x-", SX);
-    b(6, W, instrument, octave, "x---", "-x--", "--x-", "---x", SX);
-    setKey(D);
-    b(4, H, instrument, octave, "--x", "--x", "xx-", "--x", SX);
-    b(1, H, instrument, octave, "-xx", "-xx", "x--", "-xx", SX);
-    c(5, Q, instrument, octave+1);
-    c(5, Q, instrument, octave+1);
-    c(5, Q, instrument, octave);
-    c(5, Q, instrument, octave+1);
-    b(6, H, instrument, octave, "x--", "--x", "--x", "-x-", SX);
-    ci1(6, Q, instrument, octave);
-    b(6, Q, instrument, octave, "x-x", SX);
-    b(4, H, instrument, octave, "x--", "--x", "--x", "-x-", SX);
-    ci2(1, Q, instrument, octave-1);
-    b(1, Q, instrument, octave, "x-x", SX);
-    cpower(5, W, instrument, octave);*/
+    for (octave = 2; octave < 7; octave++)
+        for (i = 0; i < 7; i++)
+            n(1, I, instrument, octave, i);
 
     closeOutput();
 
     return 0;
-}
-
-void riff(int instrument, int octave)
-{
-    n(1, Q, instrument, octave+1, 0);
-    n(1, I, instrument, octave, 4);
-    n(1, I, instrument, octave, 2);
-    n(1, Q, instrument, octave+1, 0);
-    n(1, Q, instrument, octave, 4);
-
-    n(1, Q, instrument, octave, 6);
-    n(1, I, instrument, octave, 4);
-    n(1, I, instrument, octave, 4);
-    n(1, Q, instrument, octave, 6);
-    n(1, Q, instrument, octave, 4);
 }
